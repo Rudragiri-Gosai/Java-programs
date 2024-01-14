5cdc5e3b4f663dde6250529d4ca4690e19672c7b
Rudragiri_gosai <dipakgosai199@gmail.com>
2024-01-14 15:10:09 +0530

    first commit

--- chapter1/ch1pro1.java
+++ chapter1/ch1pro1.java
@@ -7,17 +7,18 @@ public class ch1pro1 {
 
         Scanner get = new Scanner(System.in);
 
-        int number=3;
-        float sum=0;
+        int number = 3;
+        float sum = 0;
         float[] num = new float[number];
 
         System.out.println("Enter Three Number :: ");
 
         for (int i = 0; i < 3; i++) {
-            System.out.print("Enter "+(i+1)+" Number : ");
-            num[i]=get.nextFloat();
-            sum+=num[i];
+            System.out.print("Enter " + (i + 1) + " Number : ");
+            num[i] = get.nextFloat();
+            sum += num[i];
         }
-        System.out.print("The sum of Given number is :: "+sum);
+        get.close();
+        System.out.print("The sum of Given number is :: " + sum);
     }
 }
--- chapter1/ch1pro2.java
+++ chapter1/ch1pro2.java
@@ -6,19 +6,20 @@ public class ch1pro2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter number of Subject : ");
-        int n=sc.nextInt();
+        int n = sc.nextInt();
         double[] marks = new double[n];
         double cgpa, sum = 0;
 
         System.out.println("Enter the marks of three subjects (out of 100):");
         for (int i = 0; i < n; i++) {
-            System.out.print("Enter Marks of "+(i+1)+" Subject : ");
+            System.out.print("Enter Marks of " + (i + 1) + " Subject : ");
             marks[i] = sc.nextDouble();
             sum += marks[i] / 10;
         }
 
         cgpa = sum / n;
+        sc.close();
+
         System.out.printf("CGPA: %.2f\n", cgpa);
     }
 }
-
